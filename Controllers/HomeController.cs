8bbee9ce1e462763322ebb5affa3dd9acb673bdd
zncampbell <52433719+zncampbell@users.noreply.github.com>
2020-11-11 11:59:58 -0600

    Add files via upload

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PriceQuotation.Models;
+ public class HomeController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            ViewBag.discount = 0;
+            ViewBag.total = 0;
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Index(PriceQuotationModel model)
+        {
+        if (ModelState.IsValid)
+        {
+            ViewBag.discount = model.CalculateDiscount();
+            ViewBag.total = model.CalculateTotal();
+        }
+        else
+        {
+            ViewBag.discount = 0;
+            ViewBag.total = 0;
+        }
+            return View(model);
+        }
+    }
---
+++ Models/PriceQuotationModel.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace PriceQuotation.Models
+{
+    public class PriceQuotationModel
+    {
+        [Required(ErrorMessage = "Please enter a value for the sales price.")]
+        [Range(0, 5000, ErrorMessage = "The sales price must be a number between 0 and 5000.")]
+        public decimal? subTotal { get; set; }
+
+        [Required(ErrorMessage = "Please enter a value for the discount percentage.")]
+        [Range(0, 100, ErrorMessage = "The percent must be between 0 and 100.")]
+        public decimal? percent { get; set; }
+        public decimal? CalculateDiscount ()
+        {
+            decimal? discount = (percent / 100) * subTotal;
+            return discount;
+        }
+        public decimal? CalculateTotal ()
+        {
+            decimal? total = subTotal - ((percent / 100) * subTotal);
+            return total;
+        }
+
+
+    }
+}
